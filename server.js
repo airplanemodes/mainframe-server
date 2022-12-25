ab33bdf9d1fc86be601dc3340d32986bfad931c0
Roy Elman <airplanemodesnetwork@gmail.com>
2022-12-25 15:32:08 +0200

    added socket.io, jwt version update

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 const { allowAccessControl, routes } = require('./routes/config');
 const mainframe = express();
 
 mainframe.use(express.json());
-
 allowAccessControl(mainframe);
 routes(mainframe);
 
+const server = http.createServer(mainframe);
+const io = socketio(server, {
+  // https://socket.io/docs/v4/handling-cors/
+  cors: {
+    origin: "http://localhost:3000",
+    // allowedHeaders: ["my-custom-header"],
+    // credentials: true
+  }
+});
+
+// Run when client connects
+io.on('connection', socket => {
+  console.log("New user connected");
+  
+})
+
 const port = process.env.PORT || 4000;
-mainframe.listen(port, () => {
+server.listen(port, () => {
     console.log(`*** Server running on port ${port}`);
 });
